cd4f5b74c1d42bed59b53ab45f470944a2effc02
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-25 17:57:38 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,9 +2,17 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  configure do
+    enable :sessions
+    set :sessions_secret, "secret"
+  end
 
+  get '/' do
+    erb :index
+  end
 
-
-
-
+  post '/checkout' do
+    session["item"] = params[:item]
+    @session = session
+  end
 end
